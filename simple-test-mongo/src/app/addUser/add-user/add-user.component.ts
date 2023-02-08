e5c76d9a7e04b6b6868b9ce38c2122ede15385e4
gigijeskai <luigindaco@gmail.com>
2023-02-08 18:03:05 +0100

    commit

--- simple-test-mongo/src/app/addUser/add-user/add-user.component.ts
+++ simple-test-mongo/src/app/addUser/add-user/add-user.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsersSrvService } from 'src/app/services/users-srv.service';
 
 @Component({
   selector: 'app-add-user',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user.component.scss'],
 })
 export class AddUserComponent implements OnInit {
+  @ViewChild('newUser') form!: NgForm;
   constructor() {}
 
   ngOnInit(): void {}
   onSubmit() {
-    console.log('ciao');
+    // this.userSrv
+    //   .addUser(this.form.value)
+    //   .subscribe((user) => this.userSrv.users?.push(user));
+    // return this.form.reset();
   }
 }
